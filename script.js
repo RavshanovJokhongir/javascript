5fe814ccfcf7abd5f3736f9fa34be46b72c30729
John <jakhonravshanov@gmail.com>
2024-02-09 15:16:06 +0900

    cat added

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ console.log();
 console.log("know");
 console.log();
 console.log("JavaScript");
+
+console.log("A cat");
+console.log();
+console.log("says");
+console.log();
+console.log("meow");
+
+ 
