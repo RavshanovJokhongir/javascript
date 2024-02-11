369fadf55a3030343c567490272c7a0404d7a3e2
John <jakhonravshanov@gmail.com>
2024-02-11 12:09:17 +0900

    else added

--- script.js
+++ script.js
@@ -2,20 +2,32 @@
 * The first program
 */
 
-console.log("Hello, World!");
-console.log("I");
-console.log();
-console.log("know");
-console.log();
-console.log("JavaScript");
+// console.log("Hello, World!");
+// console.log("I");
+// console.log();
+// console.log("know");
+// console.log();
+// console.log("JavaScript");
 
 /**
 * The second program
 */
-console.log("A cat");
-console.log();
-console.log("says");
-console.log();
-console.log("meow");
 
- 
+// console.log("A cat");
+// console.log();
+// console.log("says");
+// console.log();
+// console.log("meow");
+
+ /**
+* The third program
+*/
+
+let login = 'example';
+let password = 'dollar'
+
+if(login ==='example' && password === 'dollar'){
+    console.log('Welcome to Instagram');
+}else{
+    console.log('Login or password incorrect');
+}
