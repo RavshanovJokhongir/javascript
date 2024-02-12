cb5c3c00cbefabedfd67f428aa0f25a8aa097a41
John <jakhonravshanov@gmail.com>
2024-02-12 12:21:47 +0900

    .

--- script.js
+++ script.js
@@ -50,18 +50,18 @@
 //     console.log('unavailable');
 // }
 
-let day = 5;
-
-switch(day){
-    case 1 : console.log('Monday'); break
-    case 2 : console.log('Tuesday'); break
-    case 3 : console.log('Wednesday'); break
-    case 4 : console.log('Thursday'); break
-    case 5 : console.log('Friday'); break
-    case 6 : console.log('Saturday'); break
-    case 7 : console.log('Sunday'); break
-    default : 'No available';
-}
-
+// let day = 5;
+// switch(day){
+//     case 1 : console.log('Monday'); break
+//     case 2 : console.log('Tuesday'); break
+//     case 3 : console.log('Wednesday'); break
+//     case 4 : console.log('Thursday'); break
+//     case 5 : console.log('Friday'); break
+//     case 6 : console.log('Saturday'); break
+//     case 7 : console.log('Sunday'); break
+//     default : 'No available';
+// }
 
 
+let color = 'black';
+color === 'black' ? console.log('right') : console.log('wrong');
