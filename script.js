d0a3a6a5b953e518441b164da3bc05e2b79d3356
John <jakhonravshanov@gmail.com>
2024-02-12 11:48:32 +0900

    switch added

--- script.js
+++ script.js
@@ -31,18 +31,32 @@
 // }else{
 //     console.log('Login or password incorrect');
 // }
-let fruit = 'apple';
-let kg = 1;
+
+/*---------*/
+// let fruit = 'apple';
+// let kg = 1;
 
 
-if(fruit === 'apple'){
-    if(kg === 1){
-        console.log(1000);
-    }else if (kg === 2){
-        console.log(2000);
-    }else {
-        console.log(3000);
-    }
-}else {
-    console.log('unavailable');
+// if(fruit === 'apple'){
+//     if(kg === 1){
+//         console.log(1000);
+//     }else if (kg === 2){
+//         console.log(2000);
+//     }else {
+//         console.log(3000);
+//     }
+// }else {
+//     console.log('unavailable');
+// }
+
+let day = 5;
+
+switch(day){
+    case 1 : console.log('Monday'); break
+    case 2 : console.log('Tuesday'); break
+    case 3 : console.log('Wednesday'); break
+    case 4 : console.log('Thursday'); break
+    case 5 : console.log('Friday'); break
+    case 6 : console.log('Saturday'); break
+    case 7 : console.log('Sunday'); break
 }
