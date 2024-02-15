6ec2c2dccacc91190c465a09fbd4efc0b1c2238a
John <jakhonravshanov@gmail.com>
2024-02-15 12:53:37 +0900

    while statement added

--- script.js
+++ script.js
@@ -185,9 +185,16 @@
 // }
 
 /* do while statement */
+// let i = 0;
+// do {
+//   i += 1;
+//   console.log(i);
+// } while (i < 5);
 
-let i = 0;
-do {
-  i += 1;
-  console.log(i);
-} while (i < 5);
+let n = 0;
+let x = 0;
+while (n < 3) {
+  n++;
+  x += n;
+}
+console.log(n);
