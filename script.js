8e7a0e6758ed51211dc285f6ab16a0cd8f1097b1
John <jakhonravshanov@gmail.com>
2024-02-16 11:11:58 +0900

    continue statement added

--- script.js
+++ script.js
@@ -301,14 +301,23 @@
 // console.log(str);
 
 /* labeled break with for loops*/
-let i, j;
-loop1: for (i = 0; i < 3; i++) {
-  loop2: for (j = 0; j < 3; j++) {
-    if (i === 1 && j === 1) {
-      break loop1;
-    }
-    console.log(`i = ${i}, j = ${j}`);
+// let i, j;
+// loop1: for (i = 0; i < 3; i++) {
+//   loop2: for (j = 0; j < 3; j++) {
+//     if (i === 1 && j === 1) {
+//       break loop1;
+//     }
+//     console.log(`i = ${i}, j = ${j}`);
+//   }
+// }
+
+/* labeled continue statement*/
+for (let i = 1; i <= 5; i++) { 
+  if (i == 3) {
+      continue;
   }
+
+  console.log(i);
 }
 
 /* nested loop*/
