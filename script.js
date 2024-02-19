382090366ecae96cabc4d0e73a3dcf51ded30320
John <jakhonravshanov@gmail.com>
2024-02-19 20:45:45 +0900

    closures added

--- script.js
+++ script.js
@@ -654,15 +654,30 @@
 // }
 
 /* Name conflicts  */
-function outside() {
-  const x = 5;
-  function inside(x) {
-    return x * 2;
-  }
-  return inside;
-}
-console.log(outside()(10)); 
-// The name conflict happens at the statement return x * 2 and is between inside's parameter x and outside's variable x. 
-// The scope chain here is {inside, outside, global object}. 
-//Therefore, inside's x takes precedences over outside's x, and 20 (inside's x) is returned instead of 10 (outside's x).
+// function outside() {
+//   const x = 5;
+//   function inside(x) {
+//     return x * 2;
+//   }
+//   return inside;
+// }
+// console.log(outside()(10)); 
+// // The name conflict happens at the statement return x * 2 and is between inside's parameter x and outside's variable x. 
+// // The scope chain here is {inside, outside, global object}. 
+// //Therefore, inside's x takes precedences over outside's x, and 20 (inside's x) is returned instead of 10 (outside's x).
+
+/*  Closures   */
+// Closures are one of the most powerful features of JavaScript.
+// JavaScript allows for the nesting of functions and grants the inner function full access to all the variables and functions defined 
+//inside the outer function (and all other variables and functions that the outer function has access to).
+// The outer function defines a variable called "name"
+const pet = function (name) {
+  const getName = function () {
+    // The inner function has access to the "name" variable of the outer function
+    return name;
+  };
+  return getName; 
+};
+const myPet = pet("Vivie");
+console.log(myPet()); 
 
