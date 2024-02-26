88506839d680e4d0ceb76803e27b1ae34ff0b4ab
John <jakhonravshanov@gmail.com>
2024-02-26 20:33:29 +0900

    check if a value Array.isArray() method   added

--- script.js
+++ script.js
@@ -1403,6 +1403,14 @@
 // console.log(firstElement);
 
 /* Array indexOf() method */
-let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
-let index = seas.indexOf('North Sea');
-console.log(index); 
+// let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+// let index = seas.indexOf('North Sea');
+// console.log(index); 
+
+/*  check if a value Array.isArray() method */
+let numbers = [1, 2, 3, 4];
+// checking whether numbers is an array or not
+console.log(Array.isArray(numbers));
+let text = "JavaScript";
+// checking whether text is an array or not
+console.log(Array.isArray(text));
