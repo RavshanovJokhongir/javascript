582283dc0d387091ffedb5bcdfca04bd8c87ee7c
John <jakhonravshanov@gmail.com>
2024-03-16 21:27:39 +0900

    array advanced  destructure method in real projects

--- script.js
+++ script.js
@@ -1873,6 +1873,24 @@
 
 
 /* array destructure */
-let arr = [100, 200, 200, 400];
-let [bir, ikki, uch, turt] = arr;
-console.log(turt);
+// let arr = [100, 200, 200, 400];
+// let [bir, ikki, uch, turt] = arr;
+// console.log(turt);
+
+
+
+let cars = [
+    { id: 1, year: 1998, engine: 1, name: 'Tico'},
+    { id: 2, year: 2005, engine: 1.2, name: 'Matiz'},
+    { id: 3, year: 2010, engine: 1.6, name: 'Gentra'},
+    { id: 4, year: 2010, engine: 1.5, name: 'Cobalt'},
+    { id: 5, year: 2012, engine: 2, name: 'Malibu'},
+    { id: 6, year: 2000, engine: 1.2, name: 'Damas'},
+    { id: 7, year: 2018, engine: 2.4, name: 'Tracker'}
+];
+cars.map(({id, year, engine, name}) => {
+  console.log(name);
+  console.log(id);
+  console.log(year);
+  console.log(engine);
+});
