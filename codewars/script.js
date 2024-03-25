f2d1362265ed854f166a3b06093ed31b5173291c
John <jakhonravshanov@gmail.com>
2024-03-25 21:49:49 +0900

    codewars 2nd task

--- codewars/script.js
+++ codewars/script.js
@@ -1,6 +1,17 @@
-function removeChar(str){
+/////////1st task////////////////
+// function removeChar(str){
+//     let result = "";
+//     for (let i = 1; i < str.length - 1; i++) result += str[i];
+//     return result
+//    }
+//    console.log(removeChar("eloquent"))
+
+/////////2nd task////////////////
+function repeatStr (n, s) {
     let result = "";
-    for (let i = 1; i < str.length - 1; i++) result += str[i];
-    return result
-   }
-   console.log(removeChar("eloquent"))
+    for(let i = 0; i < n; i++) result += s;
+    return result;
+}
+    console.log(repeatStr(3, "*"));
+    console.log(repeatStr(5, "#"));
+  
