e43c86c3dd4991ea883a2a87956a251d11fbda09
John <jakhonravshanov@gmail.com>
2024-03-26 21:08:34 +0900

    codewars string 4th task

--- codewars/script.js
+++ codewars/script.js
@@ -17,15 +17,21 @@
   
 
 ////////3rd task ///////////////////////
-function noSpace(x) {
-    let result = '';
-    for(let i = 0; i < x.length; i++){
-        if(x[i] === ' ') {
-            continue;
-        }else {
-            result += x[i];
-        }
-    }
-    return result;
-}
-console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
+// function noSpace(x) {
+//     let result = '';
+//     for(let i = 0; i < x.length; i++){
+//         if(x[i] === ' ') {
+//             continue;
+//         }else {
+//             result += x[i];
+//         }
+//     }
+//     return result;
+// }
+// console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
+
+//////////4th task///////////////////////////////////
+function booleanToString(b){
+    return String(b);
+  }
+  
