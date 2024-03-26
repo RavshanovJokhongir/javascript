c3371b69d28dc8b9c98884e7c22704962242eb8f
John <jakhonravshanov@gmail.com>
2024-03-26 20:52:48 +0900

    codewars 3rd task

--- codewars/script.js
+++ codewars/script.js
@@ -7,11 +7,25 @@
 //    console.log(removeChar("eloquent"))
 
 /////////2nd task////////////////
-function repeatStr (n, s) {
-    let result = "";
-    for(let i = 0; i < n; i++) result += s;
+// function repeatStr (n, s) {
+//     let result = "";
+//     for(let i = 0; i < n; i++) result += s;
+//     return result;
+// }
+//     console.log(repeatStr(3, "*"));
+//     console.log(repeatStr(5, "#"));
+  
+
+////////3rd task ///////////////////////
+function noSpace(x) {
+    let result = '';
+    for(let i = 0; i < x.length; i++){
+        if(x[i] === ' ') {
+            continue;
+        }else {
+            result += x[i];
+        }
+    }
     return result;
 }
-    console.log(repeatStr(3, "*"));
-    console.log(repeatStr(5, "#"));
-  
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
