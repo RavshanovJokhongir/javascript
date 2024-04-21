d676caf4979d218e267c5536bd5a4255722d2068
John <jakhonravshanov@gmail.com>
2024-04-21 22:21:55 +0900

    crud example code

--- script.js
+++ script.js
@@ -2171,9 +2171,26 @@
 // console.log(function);
 
 ////////////////////////////////////////////////////////
-const months = ['March', 'Jan', 'Feb', 'Dec'];
-months.sort();
-console.log(months);
-const array1 = [1, 30, 4, 21, 100000];
-array1.sort();
-console.log(array1);
+// const months = ['March', 'Jan', 'Feb', 'Dec'];
+// months.sort();
+// console.log(months);
+// const array1 = [1, 30, 4, 21, 100000];
+// array1.sort();
+// console.log(array1);
+
+///////////////////////////////////////////////////
+const rectangle = { 
+	length: 25, 
+	width: 20, 
+	area: function () { 
+		return this.length * this.width 
+	} 
+} 
+const square = new Object(); 
+square.side = 10; 
+square.area = function () { 
+	return this.side * this.side 
+}; 
+square.side = 20; 
+console.log(rectangle, "area: " + rectangle.area()); 
+console.log(square, "area: " + square.area());
