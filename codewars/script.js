601554c6d3719d026156bdb19852b57782cea5e5
John <jakhonravshanov@gmail.com>
2024-08-08 23:34:19 +0900

    codewars merge Arrays example code

--- codewars/script.js
+++ codewars/script.js
@@ -615,13 +615,23 @@
 
 
 //////////// 55th task //////////////////////////////////////////////////////////////////////////////////////
-function fixMeerkat(arr) {
-  return arr.reverse();
-}
-const meerkatArray = ['tail', 'body', 'head'];
-const fixedArray = fixMeerkat(meerkatArray);
-console.log(fixedArray); 
+// function fixMeerkat(arr) {
+//   return arr.reverse();
+// }
+// const meerkatArray = ['tail', 'body', 'head'];
+// const fixedArray = fixMeerkat(meerkatArray);
+// console.log(fixedArray); 
 
+
+//////////// 56th task //////////////////////////////////////////////////////////////////////////////////////
+function mergeArrays(arr1, arr2) {
+  const combined = arr1.concat(arr2);
+  const unique = new Set(combined);
+  return Array.from(unique).sort((a, b) => a - b);
+}
+console.log(mergeArrays([1, 2, 3, 4, 5], [6, 7, 8, 9, 10])); 
+console.log(mergeArrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2])); 
+console.log(mergeArrays([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12])); 
 
 
 
